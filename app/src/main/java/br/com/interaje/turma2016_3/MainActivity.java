5368ce6dae89ed2440680d9516c0af282d55991a
Felipe Costa <fcostax02@gmail.com>
2016-10-17 14:04:45 -0300

    Aula 2 - Interagindo com as Views

--- app/src/main/java/br/com/interaje/turma2016_3/MainActivity.java
+++ app/src/main/java/br/com/interaje/turma2016_3/MainActivity.java
@@ -1,13 +1,39 @@
 package br.com.interaje.turma2016_3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button botaoConfirma;
+
+        email = (EditText) findViewById(R.id.usuario);
+        senha = (EditText) findViewById(R.id.senha);
+        botaoConfirma = (Button) findViewById(R.id.btn_entrar);
+
+    }
+
+    public void fazerLogin(View v) {
+        String textoEmail = email.getText().toString();
+        String textoSenha = senha.getText().toString();
+
+        if (textoEmail != "") {
+            // Toast.makeText(MainActivity.this, "Bem vindo!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, ProdutoActivity.class);
+            startActivity(intent);
+        }
     }
 }
